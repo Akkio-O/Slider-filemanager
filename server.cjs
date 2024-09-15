a57389d0f98e15193fbf930f48950faf188b93c8
Eugene <zheka12620@gmail.com>
2024-09-15 18:37:18 +0300

    fix path in server

--- server.cjs
+++ server.cjs
@@ -1,4 +1,4 @@
-const downloadFiles = require("./src//modules/nodeModules/loadRemoteFiles");
+const downloadFiles = require("./src/nodeModules/loadRemoteFiles");
 
 const fs = require("fs");
 const os = require("os");
